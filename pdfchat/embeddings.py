4b418c1cd089184dea7fab96509b16a77bf1eb3a
Monidip Das <monidipd01@gmail.com>
2026-08-01 10:05:50 +0530

    modofied

--- pdfchat/embeddings.py
+++ pdfchat/embeddings.py
@@ -1,4 +1,4 @@
-from langchain.text_splitter import CharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
